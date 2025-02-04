cd7f3ed937643538efaa92d7e95b3585935b4a3f
fizge <fiz.garrido@udc.es>
2025-02-04 02:24:36 +0100

    Refactor: folders redistribution

--- TGR1_EJ1_ABIA/obj/Debug/net9.0/Pract1.AssemblyInfo.cs
+++ TGR1_EJ1_ABIA/obj/Debug/net9.0/Pract1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pract1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f767d3cb94a030133582898c4455d01689d05d34")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pract1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pract1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
